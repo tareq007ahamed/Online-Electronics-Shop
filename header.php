d881f39e15fac1f79a36465efdc9ba8be91ce217
tareq007ahamed <fokhoruddin.tareq@gmail.com>
2022-05-21 14:36:35 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,75 @@
+
+<head>
+<title>Electronix Store</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script type="text/javascript" src="js/boxOver.js"></script>
+</head>
+<body>
+<div id="main_container">
+  <div class="top_bar">
+  
+  
+  <div id="form">
+  
+  <form method="get" action="results.php" enctype="multipart/form-data">
+  
+  
+    <div class="top_search">
+      <div class="search_text">Advanced Search</div>
+      <input type="text" class="search_input" name="user_query" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Search your need"/>
+      <input type="submit" value="Search" class="search_bt" name="search"/>
+    </div>
+    
+    
+    </form>
+    
+    </div>
+    
+    
+    <div class="languages">
+      <div class="lang_text">Languages:</div>
+        <a href="#" class="lang"><img src="images/en.gif" alt="" border="0" /></a> <a href="#" class="lang"></a> </div>
+  </div>
+  <div id="header">
+    <div id="logo"> <a href="index.php"><img src="images/intellogo.jpg" alt="" border="0" width="237" height="140" /></a> </div>
+    <div class="oferte_content">
+      <div class="top_divider"><img src="images/header_divider.png" alt="" width="1" height="164" /></div>
+      <div class="oferta">
+       <div align="center"><br><br>
+        <h1>Online Electronics Shop</h1>
+      </div>
+  
+      </div>
+
+      <div class="top_divider"><img src="images/header_divider.png" alt="" width="1" height="164" /></div>
+    </div>
+
+
+
+
+  </div>
+  <div id="main_content">
+    <div id="menu_tab">
+      <div class="left_menu_corner"></div>
+      <ul class="menu">
+        <li><a href="index.php" class="nav1"> Home</a></li>
+        <li class="divider"></li>
+        <li><a href="allproducts.php" class="nav2">All Products</a></li>
+        <li class="divider"></li>
+     
+        <li class="divider"></li>
+        <li class="divider"></li>
+        <li><a href="cart.php" class="nav5">Cart</a></li>
+        <li class="divider"></li>
+        <li class="divider"></li>
+        <li><a href="admin_area/login.html" class="nav1">Admin_login</a></li>
+        <li class="divider"></li>
+        <li class="divider"></li>
+        
+       
+        
+        
+      </ul>
+      <div class="right_menu_corner"></div>
+    </div>
---
+++ includes/db.php
@@ -0,0 +1,12 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","electronix");
+
+if(mysqli_connect_errno()){
+	
+    
+	echo"Failed to connect : " . mysqli_connect_error(); 
+	
+}
+
+?>
